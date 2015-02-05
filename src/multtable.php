486f250c8c171a72ec970bf8ed321c01e2b4431f
David Vogel <david.michael.vogel@gmail.com>
2015-02-05 13:04:17 -0500

    finished adding error-checking for parameters

--- src/multtable.php
+++ src/multtable.php
@@ -3,17 +3,51 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Content-type: text/plain');
 //http://localhost/Content/multtable.php?min-multiplicand=1&max-multiplicand=2&min-multiplier=3&max-multiplier=4
-$minMultiplicand = $_GET['min-multiplicand'];
-$maxMultiplicand = $_GET['max-multiplicand'];
-$minMultiplier = $_GET['min-multiplier'];
-$maxMultiplier = $_GET['max-multiplier'];
+//It should check that all 4 parameters exist for each missing parameter. It should print "Missing parameter [min-multiplicand ... max-multiplier]."
 
-if (($maxMultiplicand < $minMultiplicand) && preg_match('/^[0-9]+$/', $maxMultiplicand) && preg_match('/^[0-9]+$/', $minMultiplicand))  {
+//Check to see that all 4 parameters exist.
+if (isset($_GET['min-multiplicand'])) {
+    $minMultiplicand = $_GET['min-multiplicand'];    
+}
+else {
+    echo "Missing parameter min-multiplicand.\n";
+} 
+if (isset($_GET['max-multiplicand'])) {
+    $maxMultiplicand = $_GET['max-multiplicand'];
+}
+else {
+    echo "Missing parameter max-multiplicand.\n";
+}
+if (isset($_GET['min-multiplier'])) {
+    $minMultiplier = $_GET['min-multiplier'];
+}
+else {
+    echo "Missing parameter min-multiplier.\n";
+}
+if (isset($_GET['max-multiplier'])) {
+    $maxMultiplier = $_GET['max-multiplier'];
+}
+else {
+    echo "Missing parameter max-multiplier.\n";
+}
+
+//Check to see that parameters are within the correct range
+if (isset($_GET['min-multiplicand']) 
+    && isset($_GET['max-multiplicand'])
+    && $maxMultiplicand < $minMultiplicand
+    && preg_match('/^[0-9]+$/', $maxMultiplicand) 
+    && preg_match('/^[0-9]+$/', $minMultiplicand))  {
     echo "Minimum multiplicand is larger than maximum.\n";
 }
-if (($maxMultiplier < $minMultiplier) && preg_match('/^[0-9]+$/', $maxMultiplier) && preg_match('/^[0-9]+$/', $minMultiplier))  {
+if (isset($_GET['min-multiplier'])
+    && isset($_GET['max-multiplier'])
+    && $maxMultiplier < $minMultiplier
+    && preg_match('/^[0-9]+$/', $maxMultiplier) 
+    && preg_match('/^[0-9]+$/', $minMultiplier))  {
     echo "Minimum multiplier is larger than maximum.\n";
 }
+
+//Check to see that the parameters are integers
 foreach($_GET as $key => $value) {
     if (!preg_match('/^[0-9]+$/', $value)) {
         switch ($key) {
@@ -34,6 +68,7 @@ foreach($_GET as $key => $value) {
         }
     }
 }
+
 
 
 
